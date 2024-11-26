b54aa9d730f55481940e0722ccb3b508db1e6dec
Nurmayaaaaa <240306054.mhs@uinmataram.ac.id>
2024-11-26 14:09:22 +0800

    Create Stack.py

---
+++ Stack.py
@@ -0,0 +1,19 @@
+stack = []
+
+# Push elemen ke stack
+stack.append (10)
+stack.append (20)
+stack.append (30)
+print("Stack setelah push",stack)
+
+
+# Pop elemen dari stack
+elemen = stack.pop (10)
+print("Elemen yang di-pop",elemen)
+print("Stack setelah di-pop",stack)
+
+# Peek elemen teratas
+if stack:
+	print("Elemen teratas",stack[-1])
+else:
+	print("Stack kosong")
